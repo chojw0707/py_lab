07ffa6668e120028987a4719aeaa7b57da71c15c
chojw0707 <chojw0707@naver.com>
2020-04-10 13:27:43 +0900

    aver_num.py

--- aver_num.py
+++ aver_num.py
@@ -1,1 +1,20 @@
-git text
+#!/usr/bin/python
+
+num = input('how many numbers?')
+
+n = int(num)
+
+a = []
+
+for i in range(n):
+		
+	a.append(int(input()))
+	
+
+sum=0
+
+for i in range(len(a)):
+	sum = sum + a[i]
+average  = sum / len(a)
+
+print(average)
